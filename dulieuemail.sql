bb47106308e2a0cef5ed77a1299d5f3584fc5379
Nguyen Trong Quyet <1951060972@e.tlu.edu.vn>
2022-01-14 21:57:58 +0700

    sql

---
+++ dulieuemail.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `data_email`(
+`hovaten` varchar(250) not null,
+`Chude` varchar(500) not null,
+`body` varchar(9000) not null
+ )
---
+++ user_sendemail.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `user_sendemail`(
+`email` varchar(250) not null,
+`Chude` varchar(500) not null,
+`body` varchar(9000) not null
+ )
+
+
+
